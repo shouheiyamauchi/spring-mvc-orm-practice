0005f33f5dbc26adfa15d7ee50348c1c3f8fd1cb
Shouhei Yamauchi <shouhei.yamauchi@live.com>
2017-10-25 08:22:43 +1100

    add methods to controller and add view

--- src/main/java/com/shouhei/spring/springmvcorm/user/controller/UserController.java
+++ src/main/java/com/shouhei/spring/springmvcorm/user/controller/UserController.java
@@ -1,12 +1,29 @@
 package com.shouhei.spring.springmvcorm.user.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.shouhei.spring.springmvcorm.user.entity.User;
 import com.shouhei.spring.springmvcorm.user.services.UserService;
 
 @Controller
 public class UserController {
 	private UserService service;
+
+	@RequestMapping("registrationPage")
+	public String showRegistrationPage() {
+		return "userReg";
+	}
+
+	@RequestMapping(value = "registerUser", method = RequestMethod.POST)
+	public String registerUser(@ModelAttribute("user") User user, ModelMap model) {
+		int result = service.save(user);
+		model.addAttribute("result", "User created with ID: " + result);
+		return "userReg";
+	}
 
 	public UserService getService() {
 		return service;
